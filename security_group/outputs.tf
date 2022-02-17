f85bc70c86089852aa9651957bba054689d8583e
Walter Traspadini <walter.traspadini@pagopa.it>
2022-02-17 18:58:08 +0100

    fix: Readme (#3)
    * fix readme file
    
    * dummy
    
    * fix: Readme file
    
    * feat: Output descriptions

--- security_group/outputs.tf
+++ security_group/outputs.tf
@@ -1,7 +1,9 @@
 output "id" {
-  value = aws_security_group.this.id
+  value       = aws_security_group.this.id
+  description = "Security group id"
 }
 
 output "arn" {
-  value = aws_security_group.this.arn
+  value       = aws_security_group.this.arn
+  description = "Security group arn"
 }
